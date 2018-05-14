32ef85a557d16d5c5da091439bc4e0f1057e9f56
Luca Di Gaspero <l.digaspero@uniud.it>
2018-05-14 15:34:46 +0200

    This also include semantic-ui depenendencies javascript.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -25,5 +26,6 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [new webpack.ProvidePlugin({ $: "jquery", jQuery: "jquery" })]
 };
